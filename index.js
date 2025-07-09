a9ca6c01362523f80d186ecca9d696ac25b6b735
Bung <android.sitko@gmail.com>
2025-07-09 15:59:48 -0700

    Set up basic React framework using vite

--- index.js
+++ index.js
@@ -9,4 +9,18 @@ const response = await client.responses.create({
     input: "Write a one sentence bedtime story about a unicorn"
 });
 
-console.log(response.output_text);
+const imageResponse = await client.responses.create({
+    model: "gpt-4.1-mini",
+    input: [{
+        role: "user",
+        content: [
+            { type: "input_text", text: "Declare what is in the image.Use environmentally sustainable practices to suggest a change to the habits in this image." },
+            {
+                type: "input_image",
+                image_url: "https://sepurahome.com/cdn/shop/articles/run-water-with-garbage-disposal_1024x1024.png?v=1684173981",
+            },
+        ],
+    }],
+});
+
+//console.log(imageResponse.output_text);
