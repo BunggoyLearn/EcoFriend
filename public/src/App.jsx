7664f536e5665b5bd09f83f1da21ce6a72110988
Bung <android.sitko@gmail.com>
2025-07-10 22:39:16 -0700

    base project functionality complete

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <>
       <div style={{ padding: '2rem' }}>
-      <h1>Ask OpenAI</h1>
+      <h1>EcoFriend</h1>
       <form onSubmit={handleSubmit}>
         <textarea
           rows="4"
